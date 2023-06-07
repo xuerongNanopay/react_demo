1f0535d1352f33d53e367938d75298484b9447d7
xuerongNanopay <xuerong@nanopay.net>
2023-06-07 12:16:29 -0400

    refact DigitalInputField into UI component

---
+++ nano-folk/src/component/ui/digital_input_field/DigitalInputField.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import css from './DigitalInputField.module.css'
+
+const DigitalInputField = ({submit=undefined, digits=6, supportAlpha=false}) => {
+  const [codeArr, setCodeArr] = useState(Array(digits).fill(""))
+  const refs = Array(digits).fill(null)
+
+  const codeChange = (idx) => {
+    return (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log("aaa")
+      let keyCode = e.keyCode
+      //Handle delete key event.
+      if ( keyCode === 8 ) {
+        if ( idx === 0 && codeArr[idx] === "" ) return; 
+        if ( codeArr[idx] !== "" ) codeArr[idx] = "";
+        else if ( idx - 1 >= 0 && codeArr[idx-1] !== "" ) {
+          codeArr[idx-1] = "";
+          refs[idx-1].select() 
+        }
+      } else {
+        if (
+          ! (
+            (keyCode >= 48 && keyCode <= 57) ||  
+            (supportAlpha &&
+              (
+                (keyCode >= 65 && keyCode <= 90) || 
+                (keyCode>=97 && keyCode<=122)
+              )
+            )
+          )
+        ) return
+        codeArr[idx] = e.key
+        if ( idx + 1 < digits) refs[idx+1].select() 
+      }
+      setCodeArr([...codeArr])
+      if ( submit !== undefined ) submit(codeArr.join(''))
+    }
+  }
+
+  return (
+    <div
+      className={`${css.digitalInputField}`}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '1rem',
+        // padding: '2rem'
+      }}
+    > 
+      {
+        codeArr.map((val, idx) => {
+          return <input ref={(e) => refs[idx] = e} key={idx} value={val} onChange={_ => {}} onKeyUp={codeChange(idx)} type='text'/>
+        })
+      }
+    </div>
+  )
+
+}
+
+export default DigitalInputField
--- nano-folk/src/component/unauth/VerifyEmail.js
+++ nano-folk/src/component/unauth/VerifyEmail.js
@@ -1,48 +1,26 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import nbp_logo from 'image/nbp_foree_remittance_logo.svg'
 import Button from 'react-bootstrap/Button'
+import DigitalInputField from 'component/ui/digital_input_field/DigitalInputField'
 import css from './VerifyEmail.module.scss'
 
 import {
   Link
 } from 'react-router-dom'
 
-const VERIFY_DIGIT_SIZE = 6;
+const VERIFY_DIGIT_SIZE = 6
 
 const VerifyEmail = _ => {
-  const [code, setCode] = useState(Array(VERIFY_DIGIT_SIZE).fill(""))
-  const refs = Array(VERIFY_DIGIT_SIZE).fill(null)
   const [canSubmit, setCanSubmit] = useState(false)
-  const codeMemo = useMemo(() => [...code], [code]);
-
-  const codeChange = (idx) => {
-    return (e) => {
-      e.preventDefault();
-      let keyCode = e.keyCode
-      if ( keyCode === 8 ) {
-        if ( code[idx] !== "" ) code[idx] = "";
-        else if ( idx - 1 >= 0 && code[idx-1] !== "" ) {
-          code[idx-1] = "";
-          refs[idx-1].select() 
-        }
-      } else {
-        if ( keyCode < 48 || keyCode > 57 ) return
-        if ( idx + 1 < VERIFY_DIGIT_SIZE) refs[idx+1].select() 
-        code[idx] = e.key
-      }
-      setCode([...code])
-
-    }
-  }
+  const [code, setCode] = useState("")
 
   useEffect(() => {
-    const sendCode = codeMemo.join('')
-    if ( sendCode.length === VERIFY_DIGIT_SIZE ) setCanSubmit(true)
+    if ( code.length === VERIFY_DIGIT_SIZE ) setCanSubmit(true)
     else setCanSubmit(false)
-  }, [codeMemo])
+  }, [code])
 
   const submitVerifyCode = _ => {
-    alert("TODO: Submit Code: " + code.join(""))
+    alert("TODO: Submit Code: " + code)
   }
 
   const resendCode = _ => {
@@ -67,21 +45,11 @@ const VerifyEmail = _ => {
           <p className="text-center">We have sent a verification code to your email. Please enter the code below to confirm that this account belongs to you.</p>
           <div>
             <p style={{textAlign:'center'}}>Verification Code</p>
-            <div
-              className={`${css.verifyCode}`}
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                gap: '1rem',
-                // padding: '2rem'
-              }}
-            > 
-              {
-                code.map((val, idx) => {
-                  return <input ref={(e) => refs[idx] = e} key={idx} value={val} onChange={_ => {}} onKeyUp={codeChange(idx)} type='text'/>
-                })
-              }
-            </div>
+            <DigitalInputField 
+              submit={val => setCode(val)}
+              digits={VERIFY_DIGIT_SIZE}
+              supportAlpha={true}
+            />
             <Button
               onClick={submitVerifyCode}
               style={{margin: 'auto', display: 'block', width: '70%'}}
