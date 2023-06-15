772853a913d1c272cd69d0e00e1fd1f64ceb5cea
xuerongNanopay <xuerong@nanopay.net>
2023-06-14 21:33:59 -0400

    add HambugerIcon

--- nano-folk/src/component/views/menu-view/MenuView.js
+++ nano-folk/src/component/views/menu-view/MenuView.js
@@ -1,14 +1,12 @@
 import css from './MenuView.module.css'
 
-import { GiHamburgerMenu } from "react-icons/gi"
-
 const MenuView = () => {
   return (
     <div className={`${css.container}`}>
       <nav className={`${css.navbar}`}>
         <div className={`${css.navBarInner}`}>
           <div className={`${css.navBarItems}`}>
-            <GiHamburgerMenu/>
+            <HambugerIcon/>
             <a href='/#' className={`${css.navBarItem}`}>Demo1</a>
             <a href='/#' className={`${css.navBarItem}`}>Demo2</a>
           </div>
@@ -19,6 +17,23 @@ const MenuView = () => {
         </div>
       </nav>
     </div>
+  )
+}
+
+const HambugerIcon = ({size}) => {
+  return (
+    <svg 
+      stroke="currentColor" 
+      fill="currentColor" 
+      stroke-width="0" 
+      viewBox="0 0 512 512" 
+      height={!!size ? size: "2rem"}
+      width={!!size ? size: "2rem"}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z">
+      </path>
+    </svg>
   )
 }
 
