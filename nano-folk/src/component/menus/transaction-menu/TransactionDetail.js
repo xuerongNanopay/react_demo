8f3ff3e61f25df262208667ded780ed8f2635df6
xuerongNanopay <xuerong@nanopay.net>
2023-06-23 09:53:28 -0400

    add mock data for TransactionDetail

--- nano-folk/src/component/menus/transaction-menu/TransactionDetail.js
+++ nano-folk/src/component/menus/transaction-menu/TransactionDetail.js
@@ -1,10 +1,27 @@
 import { useParams } from "react-router-dom";
 
+// import css from './TransactionDetail.module.css'
+
+const EXAMPLE_DATA = {
+  id: '6b99ac9c3993e74e',
+  transactionSummary: '$28.22 → 4,109.40 | John Doe → TTTT 3000099519 TTTT',
+  sourceAccount: 'from...',
+  destinationAccout: 'to...',
+  amount: '4,109.40 PKR',
+  status: 'Send',
+  transactionFee: '205.47 PKR',
+  amountToBeReceive: '205.47 PKR',
+  amountToBeDebit: '$111119.22 CAD',
+  createAt: new Date().toISOString()
+}
 const TransactionDetail = () => {
   const { transactionId } = useParams();
   return (
     <div>
       <h1>Transaction Detail: {transactionId}</h1>
+      <div className={`{css.transactionDetailContainer}`}>
+
+      </div>  
     </div>
   )
 }
